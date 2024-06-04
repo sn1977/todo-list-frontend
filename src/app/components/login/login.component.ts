6741339b0e4e1b9fee42483cc847921c7438cc02
Sascha Nemeth <sascha.nemeth@me.com>
2024-06-04 17:41:12 +0200

    3rd commit

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+    selector: "app-login",
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: "./login.component.html",
+    styleUrl: "./login.component.scss",
 })
 export class LoginComponent {
+    username: string = "";
+    password: string = "";
 
+    async login() {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            username: this.username,
+            password: this.password,
+        });
+
+        const requestOptions: RequestInit = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow",
+        };
+        try {
+            let resp = await fetch(
+                "http://127.0.0.1:8000/login/",
+                requestOptions
+            );
+            let json = await resp.json();
+            localStorage.setItem("token", json.token);
+            // TODO: Redirect
+        } catch (error) {
+            console.error(error);
+        }
+    }
 }
